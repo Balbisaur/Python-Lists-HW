c199f57aaf4b2973ad6addf21374fdc4dc66cfbd
Balbisaur <albi3479@yahoo.com>
2024-04-14 12:12:40 -0500

    first commit

---
+++ Pyhton_List_Transformation_Task_1_2_3.py
@@ -0,0 +1,23 @@
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+
+print(grades)
+print('sorting grades from highest to lowest')
+grades.sort(reverse = True)
+print(grades)
+
+total =sum(grades)
+print('sum of grades list:', total)
+quotient = 846 / 10
+print(quotient)
+
+print('Failed')
+grades[7] = 'Failed'
+print(grades)
+
+print('Failed')
+grades[8] = 'Failed'
+print(grades)
+
+print('Failed')
+grades[9] = 'Failed'
+print(grades)
